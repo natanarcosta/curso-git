1123d1aa6ade38073d5310ed40e28681b4f2feb3
Natan Arruda Costa <natanarcosta@gmail.com>
2021-03-11 20:34:45 -0300

    120 Exericio resolvido 2/3 Enumeracoes e Composicoes

---
+++ projeto_teste/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projeto_teste.Entities.Enums
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
--- projeto_teste/Program.cs
+++ projeto_teste/Program.cs
@@ -9,20 +9,7 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
-            Order order = new Order
-            {
-                Id = 1080,
-                Moment = DateTime.Now,
-                Status = OrderStatus.PendingPayment,
-            };
-            Console.WriteLine(order);
-
-            string txt = OrderStatus.PendingPayment.ToString();
-
-            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
-
-            Console.WriteLine(txt);
-            Console.WriteLine(os);
+            
         }
     }
 }
